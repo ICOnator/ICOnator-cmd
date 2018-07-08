ed70236c9455a745fc9289aa25d0a6551c79b1b0
tbocek <tom@tomp2p.net>
2018-07-08 21:41:15 +0200

    added CSV output for public keys only

--- key800/keygen.go
+++ key800/keygen.go
@@ -16,23 +16,27 @@ import (
 	"time"
 	"encoding/binary"
 	"bytes"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
 
 	var (
-		numberOfKeys     int
-		random1     int
-		random2     int
-		random3     int
-		random4     int
+		numberOfKeys    int
+		random1     	int
+		random2     	int
+		random3     	int
+		random4     	int
+		privateKeyCSV 	string
+		publicKeyCSV 	string
 	)
 
 	flag := pflag.NewFlagSet("keygen", pflag.ExitOnError)
 
 	flag.Usage = func() {
 		println("Usage:")
-		println("  keygen [-n num] [-r1 random1] [-r2 random2] [-r3 random3] [-r4 random4]")
+		println("  keygen [-n num] [-s private-key CSV] [-p public-key CSV] [-r1 random1] [-r2 random2] [-r3 random3] [-r4 random4]")
 		println()
 		flag.PrintDefaults()
 		println()
@@ -43,15 +47,47 @@ func main() {
 	flag.IntVarP(&random2, "random2", "2", 0, "Optional random number to increase entropy")
 	flag.IntVarP(&random3, "random3", "3", 0, "Optional random number to increase entropy")
 	flag.IntVarP(&random4, "random4", "4", 0, "Optional random number to increase entropy")
+	flag.StringVarP(&privateKeyCSV, "private-keys", "s", "", "Specify a file name to store the private keys in a CSV")
+	flag.StringVarP(&publicKeyCSV, "public-keys", "p", "", "Specify a file name to store the public keys only in a CSV")
 	flag.Parse(os.Args[1:])
 
-	fmt.Printf("Private Key,Private Key (WIF),Ethereum Address,Bitcoin Address (SegWit-Bech32),Bitcoin Address (P2PKH-Base58)\n")
+	line:= fmt.Sprintf("Private Key,Private Key (WIF),Ethereum Address,Bitcoin Address (SegWit-Bech32),Bitcoin Address (P2PKH-Base58)\n")
+	if len(privateKeyCSV) > 0 {
+		ioutil.WriteFile(privateKeyCSV, []byte(line), 0644)
+	} else {
+		print(line)
+	}
+
+	if len(publicKeyCSV) > 0 {
+		linePublic:= fmt.Sprintf("Ethereum Address,Bitcoin Address (SegWit-Bech32),Bitcoin Address (P2PKH-Base58)\n")
+		ioutil.WriteFile(publicKeyCSV, []byte(linePublic), 0644)
+	}
 
 	seed := createSeed(random1, random2, random3, random4)
 	rand1 := rand.Reader //the crypto random
 	rand2 := rand2.New(rand2.NewSource(time.Now().Unix() ^ seed)) //user provided entropy
-
 	rand3 := New(rand1, rand2)
+
+
+	var fpriv *os.File;
+	if len(privateKeyCSV) > 0 {
+		var err error
+		fpriv, err = os.OpenFile(privateKeyCSV, os.O_APPEND|os.O_WRONLY, 0644)
+		defer fpriv.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	var fpub *os.File;
+	if len(publicKeyCSV) > 0 {
+		var err error
+		fpub, err = os.OpenFile(publicKeyCSV, os.O_APPEND|os.O_WRONLY, 0644)
+		defer fpub.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
 	for i := 0; i < numberOfKeys; i++ {
 		key, _ := ecdsa.GenerateKey(secp256k1.S256(), rand3)
@@ -62,7 +98,17 @@ func main() {
 
 		btcWitAddr, _ := btcutil.NewAddressWitnessPubKeyHash(btcutil.Hash160(wif.SerializePubKey()), &chaincfg.MainNetParams);
 		btcAddr, _ := btcutil.NewAddressPubKeyHash(btcutil.Hash160(wif.SerializePubKey()), &chaincfg.MainNetParams);
-		fmt.Printf("%v,%v,%v,%v,%v\n", common.ToHex(crypto.FromECDSA(key)), wif, ethAddr.Hex(), btcWitAddr, btcAddr)
+		line := fmt.Sprintf("%v,%v,%v,%v,%v\n", common.ToHex(crypto.FromECDSA(key)), wif, ethAddr.Hex(), btcWitAddr, btcAddr)
+		if len(privateKeyCSV) > 0 {
+			fpriv.WriteString(line)
+		} else {
+			print(line)
+		}
+
+		if len(publicKeyCSV) > 0 {
+			linePublic := fmt.Sprintf("%v,%v,%v\n", ethAddr.Hex(), btcWitAddr, btcAddr)
+			fpub.WriteString(linePublic)
+		}
 	}
 }
 
